2886a791914ab0b75f91c1d895ca3b8cf05c9261
epaschke22 <ericksonpaschke@gmail.com>
2021-02-03 17:46:17 +0000

    updating task 6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,6 +1,25 @@
 #include "sort.h"
 
 /**
+ * isArraySorted - checks if array is already sorted
+ * @array: input array
+ * @size: size of array
+ * Return: 1 if true, else 0
+ */
+int isArraySorted(int *array, int size)
+{
+	int sorted = 1, i;
+
+	for (i = 1; sorted == 1 && i < size; i++)
+	if (array[i] < array[i - 1])
+		sorted = 0;
+	if (sorted == 1)
+		return (1);
+	else
+		return (0);
+}
+
+/**
  * counting_sort - sorts int array with counting sort method
  * @array: input array
  * @size: size of array
@@ -12,6 +31,8 @@ void counting_sort(int *array, size_t size)
 	int max = array[0], position, *count = NULL, *output = NULL;
 
 	if (size == 0 || size == 1 || array == NULL)
+		return;
+	if (isArraySorted(array, size))
 		return;
 	for (i = 0; i < size; i++)
 		if (array[i] > max)
